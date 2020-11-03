bf07c52b92a9fb5c22e7757198bb79f5eb816430
Tristan Cole <tmcole93@gmail.com>
2020-11-03 14:43:06 -0500

    Add script for launching MP and update gitignore

---
+++ mp.bat
@@ -0,0 +1,9 @@
+START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject /Game/StarterContent/Maps/Minimal_Default -server -game -log
+
+TIMEOUT 5
+
+START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject 10.0.0.142 -game -WINDOWED ResX=1280 ResY=720 -log
+
+TIMEOUT 5
+
+START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject 10.0.0.142 -game -WINDOWED ResX=1280 ResY=720 -log
