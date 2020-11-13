3cc35cf6efd0bd095deabd5bb51b8d60bbcc7166
Tristan Cole <tmcole93@gmail.com>
2020-11-13 13:05:43 -0500

    Update quick launch script

--- mp.bat
+++ mp.bat
@@ -1,9 +1,1 @@
-START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject /Game/maps/da_track -server -game -log
-
-TIMEOUT 5
-
-START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject 10.0.0.142 -game -WINDOWED ResX=1280 ResY=720 -log
-
-TIMEOUT 5
-
-START F:\Engines\Unreal\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe F:\GameDev\Unreal\MP_Pawn_Racing\MP_Pawn_Racing.uproject 10.0.0.142 -game -WINDOWED ResX=1280 ResY=720 -log
+START Build\WindowsNoEditor\MP_Pawn_Racing.exe -WINDOWED ResX=1280 ResY=720 -log
