080b4e2163656ddfe81d8a34395684a919ed1488
Tristan Cole <tmcole93@gmail.com>
2020-11-14 13:50:01 -0500

    Add substepping with MMT plugin

---
+++ Plugins/MMT_Plugin/Source/MMT/Public/MMTSuspensionStackComponent.h
@@ -0,0 +1,26 @@
+//Copyright(c) 2017 Viktor Kuropiatnyk "BoredEngineer"
+
+#pragma once
+
+#include "Components/SceneComponent.h"
+#include "MMTSuspensionStack.h"
+#include "MMTSuspensionStackComponent.generated.h"
+
+
+UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MMT_API UMMTSuspensionStackComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMMTSuspensionStackComponent(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(VisibleAnywhere, Instanced, BlueprintReadWrite)
+	UMMTSuspensionStack* SuspensionStack;
+	
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+};
